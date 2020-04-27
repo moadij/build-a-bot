c8d2d2bca2d3fefffb27a6fb64acbf0423a66e19
moadij <moalstigsson@gmail.com>
2020-04-27 16:39:41 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+body{
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed ;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+main{
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <main>
+      <HomePage/>
+    </main>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomePage from '@/components/HomePage.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HomePage
+  }
+}
+</script>
